8be026d78e8c5cea141b7f3630e859a52642eb6a
Ulas Tuerkmen <ulas@goodscloud.com>
2015-12-20 05:57:11 +0100

    Week 11

---
+++ Week11/SExpr.hs
@@ -0,0 +1,56 @@
+module SExpr where
+
+import AParserNew
+import Control.Applicative
+import Data.Char
+
+------------------------------------------------------------
+--  1. Parsing repetitions
+------------------------------------------------------------
+
+
+
+zeroOrMore :: Parser a -> Parser [a]
+zeroOrMore p =  oneOrMore p <|> pure []
+
+
+oneOrMore :: Parser a -> Parser [a]
+oneOrMore p = ((:) <$> p) <*> zeroOrMore p
+
+------------------------------------------------------------
+--  2. Utilities
+------------------------------------------------------------
+
+spaces :: Parser String
+spaces = zeroOrMore $ satisfy isSpace
+
+ident :: Parser String
+ident = (++) <$> oneOrMore (satisfy isAlpha) <*> zeroOrMore (satisfy isAlphaNum)
+
+
+------------------------------------------------------------
+--  3. Parsing S-expressions
+------------------------------------------------------------
+
+-- An "identifier" is represented as just a String; however, only
+-- those Strings consisting of a letter followed by any number of
+-- letters and digits are valid identifiers.
+type Ident = String
+
+-- An "atom" is either an integer value or an identifier.
+data Atom = N Integer | I Ident
+  deriving Show
+
+-- An S-expression is either an atom, or a list of S-expressions.
+data SExpr = A Atom
+           | Comb [SExpr]
+  deriving Show
+
+parseAtom :: Parser Atom
+parseAtom = (N <$> posInt) <|> (I <$> ident)
+
+parsePar :: Parser SExpr
+parsePar = char '(' *> (Comb <$> oneOrMore (spaces *> parseSExpr)) <* char ')'
+
+parseSExpr :: Parser SExpr
+parseSExpr = parsePar <|> (A <$> parseAtom)
